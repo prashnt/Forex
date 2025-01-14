815560d61038a5d99a5961f7c1f9e9181fbd9498
Prashant <prashant.chaudhari@cloudmoyo.com>
2025-01-14 18:51:34 +0530

    changes

--- routes/forexRoutes.js
+++ routes/forexRoutes.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 const {sendSignal} = require("../controllers/forexController");
-const {shortsendSignal} = require("../controllers/shortController");
 router.post("/forex",sendSignal);
-router.post("/short",shortsendSignal);
 
 module.exports= router;
