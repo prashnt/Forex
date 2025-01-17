436e1b6b4af1cc1c2a3eeb6b2dfc8c904dc04328
Prashant <prashant.chaudhari@cloudmoyo.com>
2025-01-17 21:03:47 +0530

    as

--- controllers/forexController.js
+++ controllers/forexController.js
@@ -3,6 +3,7 @@ const { getToken, getAccount, sendOrder } = require('../services/mt5Service');
 exports.sendSignal = async(req,res)=>{
     const { Symbol, Signal, Volume, user, password, server } = req.body;
     const id = await getToken(user, password, server);
+    console.log(id);
     try{
         res.status(200).json({msg:`Account detials ${
             await sendOrder(id, Symbol, Signal, Volume)}`});
