373ad262b970d29ade2d6685f64cf2579a76d105
Prashant <prashant.chaudhari@cloudmoyo.com>
2025-04-30 09:21:27 +0530

    sdf

--- services/mt5Service.js
+++ services/mt5Service.js
@@ -168,11 +168,11 @@ const openOrder = async (id, Symbol, operation, Volume) => {
         let stoploss = 0.0;
         let takeprofit = 0.0;
         if (operation === 'Buy') {
-            stoploss = Symbol === 'XAUUSDm' ? quote.data.ask - 7 : quote.data.ask - 1000;
+            stoploss = Symbol === 'XAUUSDm' ? quote.data.ask - 12 : quote.data.ask - 1000;
             takeprofit = quote.data.ask + 2100;
         }
         if (operation === 'Sell') {
-            stoploss = Symbol === 'XAUUSDm' ? quote.data.ask + 7 : quote.data.bid + 1000;
+            stoploss = Symbol === 'XAUUSDm' ? quote.data.ask + 12 : quote.data.bid + 1000;
             takeprofit = quote.data.bid - 2100;
         }
         const params = {
